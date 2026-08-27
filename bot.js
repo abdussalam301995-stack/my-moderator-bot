e3248efff8594ba3229fe0707601c1fcac713364
abdussalam301995-stack <abdussalam30.1995@gmail.com>
2026-08-27 18:20:19 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running safely!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 require('dotenv').config();
 const { Telegraf, Markup } = require('telegraf');
 
